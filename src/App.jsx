469b1938a1311e7f2171a54293273f537301c7ed
Abanoub-Louka <abanoub.louka94@gmail.com>
2025-05-24 01:36:58 +0300

    modify

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,8 @@
 import "./App.css";
 import Home from "./pages/homPage/HomPage";
-import { Routes, Route } from "react-router";
-import Products from "./pages/productsPage/Products";
-import About from "./pages/aboutPage/About";
-import CosmeticPage from "./pages/cosmetic product page/CosmeticProductPage.jsx";
-import ElectronicPage from "./pages/electronic product page/ElectronicProductPage.jsx";
-import FoodPage from "./pages/food product page/FoodProductPage.jsx";
+
 function App() {
-	return (
-		<>
-			<Routes>
-				<Route path="homepage" element={<Home />} />
-				<Route path="products" element={<Products />} />
-				<Route path="about" element={<About />} />
-				<Route path="foodProdcut" element={<FoodPage />} />
-				<Route path="electronicProdcut" element={<ElectronicPage />} />
-				<Route path="cosmaticProdcut" element={<CosmeticPage />} />
-			</Routes>
-		</>
-	);
+	return <Home />;
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,23 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router";
 import App from "./App.jsx";
+import Home from "./pages/homPage/HomPage";
+import Products from "./pages/productsPage/Products";
+import About from "./pages/aboutPage/About";
+import CosmeticPage from "./pages/cosmetic product page/CosmeticProductPage.jsx";
+import ElectronicPage from "./pages/electronic product page/ElectronicProductPage.jsx";
+import FoodPage from "./pages/food product page/FoodProductPage.jsx";
 
 createRoot(document.getElementById("root")).render(
 	<BrowserRouter>
-		<App />
+		<Routes>
+			<Route path="/" element={<App />} />
+			<Route path="homepage" element={<Home />} />
+			<Route path="products" element={<Products />} />
+			<Route path="about" element={<About />} />
+			<Route path="foodProdcut" element={<FoodPage />} />
+			<Route path="electronicProdcut" element={<ElectronicPage />} />
+			<Route path="cosmaticProdcut" element={<CosmeticPage />} />
+		</Routes>
 	</BrowserRouter>
 );
